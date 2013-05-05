5fbc086104e2fb0e141ef1c9eaad320f4d6808cc
Aleksandar Gargenta <sasa@gargenta.com>
2013-05-05 02:18:35 -0700

    Added the user-specfic SQL

--- src/main/config/init-mysql.sql
+++ src/main/config/init-mysql.sql
@@ -5,3 +5,16 @@ CREATE TABLE IF NOT EXISTS category (
 	PRIMARY KEY  (id),
 	UNIQUE KEY name (name)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS user (
+    id BIGINT(20) NOT NULL AUTO_INCREMENT,
+    firstName VARCHAR(20) NOT NULL,
+    lastName VARCHAR(20) NOT NULL,
+    organization VARCHAR(64),
+    title VARCHAR(64),
+    email VARCHAR(64) NOT NULL,
+    password VARCHAR(32),
+    created DATETIME NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY email (email)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
