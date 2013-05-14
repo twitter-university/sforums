bb11ba06c287b2c746daccb12bc4e0a8f3145717
Aleksandar Gargenta <sasa@gargenta.com>
2013-05-14 13:27:41 -0700

    Removed @Past

--- src/main/java/com/marakana/sforums/domain/TimestampedEntity.java
+++ src/main/java/com/marakana/sforums/domain/TimestampedEntity.java
@@ -7,7 +7,6 @@ import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.validation.constraints.Past;
 
 @MappedSuperclass
 public abstract class TimestampedEntity extends IdentifiableEntity {
@@ -16,7 +15,6 @@ public abstract class TimestampedEntity extends IdentifiableEntity {
 
     private Date created;
 
-    @Past
     @Column(nullable = false, updatable = false)
     @Temporal(TemporalType.TIMESTAMP)
     public Date getCreated() {
